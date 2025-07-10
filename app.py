fd7ea5f1a82bac3677b6ced0858ebdeb338eeaef
Tarun Alur Gowri Shankar <160299626+tarunag23@users.noreply.github.com>
2025-07-10 14:15:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """
-Electronics Tools Hub - Maketronics Tech Challenge
+Electronics Tools Hub 
 Author: Tarun (Electronics Engineering Student)
 Description: Web application to help find electronics engineering tools
 """
